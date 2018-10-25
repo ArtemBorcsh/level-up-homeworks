6a089dd9d76c150d7943b72634f512fbf07fa121
Artem <artemborcsh69@gmail.com>
2018-10-25 22:54:54 +0100

    js 1

---
+++ public/views/javascript/homework-1/js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var name = 'Василий';
+var admin = name;
+console.log(admin);
+
+var ourPlanet = 'Земля';
+var userName = 'Артем';
+console.log(ourPlanet, userName);
+
+var fps, symbolBetweenWords, viewMode, requiredViewLastPage;
